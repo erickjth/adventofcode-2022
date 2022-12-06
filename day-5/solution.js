c5c0b666b05f9f436ee0bf2ad0fd7bf6e96e6d04
Erick Torres <ericktorresh@gmail.com>
2022-12-06 16:48:10 -0500

    add day5 part 2

--- day-5/solution.js
+++ day-5/solution.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const readline = require('readline');
 
-async function processCrates(stacks, input) {
+async function * readLines(input) {
   const fileStream = fs.createReadStream(input);
 
   const rl = readline.createInterface({
@@ -10,22 +10,32 @@ async function processCrates(stacks, input) {
   });
 
   for await (const line of rl) {
-	const [,count,from,to] = line.match(/move (\d+) from (\d+) to (\d+)/)
-
-	const fromIndex = +from - 1;
-	const toIndex = +to - 1;
-	const countNumber = +count;
-	console.log(line)
-	const droppedCreates = stacks[fromIndex].splice(-countNumber, countNumber);
-	droppedCreates.reverse();
-	stacks[toIndex].push(...droppedCreates);
-	console.table(stacks);
+    const [,count,from,to] = line.match(/move (\d+) from (\d+) to (\d+)/)
+    yield { from: +from - 1, to: +to - 1, count: +count, line }
   }
-
-  const resultantTop = stacks.map(stack => stack.slice(-1)[0]);
-
-  console.log(resultantTop.join(''));
 }
+
+async function part1(stacks, input) {
+  const copy = JSON.parse(JSON.stringify(stacks));
+
+  for await (const { count, from, to } of readLines(input)) {
+    const droppedCreates = copy[from].splice(-count, count);
+    droppedCreates.reverse();
+    copy[to].push(...droppedCreates);
+  }
+  console.log(copy.map(stack => stack.slice(-1)[0]).join(''));
+}
+
+async function part2(stacks, input) {
+  const copy = JSON.parse(JSON.stringify(stacks));
+
+  for await (const { count, from, to } of readLines(input)) {
+      const droppedCreates = copy[from].splice(-count, count);
+      copy[to].push(...droppedCreates);
+  }
+  console.log(copy.map(stack => stack.slice(-1)[0]).join(''));
+}
+
 
 
 /**
@@ -39,17 +49,20 @@ async function processCrates(stacks, input) {
 [W] [B] [H] [F] [L] [F] [J] [V] [B]
  1   2   3   4   5   6   7   8   9
  */
-
 const stacks = [
-	['W', 'M', 'L', 'F'],
-	['B', 'Z', 'V', 'M', 'F'],
-	['H', 'V', 'R','S', 'L', 'Q' ],
-	['F', 'S', 'V', 'Q', 'P', "M", 'T', 'J'],
-	['L', 'S', 'W'],
-	['F','V','P','M','R','J','W'],
-	['J','Q','C', 'P','N','R','F'],
-	['V','H','P','S','Z','W','R','B'],
-	['B','M','J','C','G','H','Z','W']
+    ['W', 'M', 'L', 'F'],
+    ['B', 'Z', 'V', 'M', 'F'],
+    ['H', 'V', 'R','S', 'L', 'Q' ],
+    ['F', 'S', 'V', 'Q', 'P', "M", 'T', 'J'],
+    ['L', 'S', 'W'],
+    ['F','V','P','M','R','J','W'],
+    ['J','Q','C', 'P','N','R','F'],
+    ['V','H','P','S','Z','W','R','B'],
+    ['B','M','J','C','G','H','Z','W']
 ];
 
-processCrates(stacks, 'input.txt');
+(async function() {
+  await part1(stacks, 'input.txt');
+  await part2(stacks, 'input-2.txt');
+})()
+
